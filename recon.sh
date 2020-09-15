f8adc6b63fae5c0594fd7b18ad66710de13a063b
3liibrah <70208387+3liibrah@users.noreply.github.com>
2020-09-15 18:55:42 +0300

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -8,7 +8,7 @@ GoSpider
 ~/go/bin/assetfinder --subs-only $1 | tee -a domains.txt
 
 #SubDomainizer
-cd SubDomainizer/python3 SubDomainizer.py -u $1 -o domains.txt | httpx -silent
+cd SubDomainizer/python3 SubDomainizer.py -u $1 -o domains.txt 
 
 #subscraper
 #python3 subscraper.py -u $1 -o domains.txt -v#
@@ -16,11 +16,11 @@ cd SubDomainizer/python3 SubDomainizer.py -u $1 -o domains.txt | httpx -silent
 
 #subfinder
 
-subfinder -d $1 -o domains.txt -v | httpx -silent
+subfinder -d $1 -o domains.txt -v 
 
 #github-subdomains
 
-cd github-search/python3 github-subdomains.py -t "3e1aa2e3f7951af257c881698aeb9d71993b5aa8" -d $1 | httpx -silent
+cd github-search/python3 github-subdomains.py -t "3e1aa2e3f7951af257c881698aeb9d71993b5aa8" -d $1 
 
 #shosubgo
 #cd ali/shosubgo; go run main.go -d $1 -s EVRbJZ2IH28JLUQggYN13JjkJmOQrEiX#
@@ -30,7 +30,7 @@ cd github-search/python3 github-subdomains.py -t "3e1aa2e3f7951af257c881698aeb9d
 
 
 #amass
-amass enum -brute -d $1  -o domains.txt | httpx -silent
+amass enum -brute -d $1  -o domains.txt 
 
 
 #removing duplicate entries
