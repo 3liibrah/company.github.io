3e6139db6012d0486ab3dde735fbbb5eede49562
3liibrah <70208387+3liibrah@users.noreply.github.com>
2020-09-17 15:40:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,70 @@
 #!/bin/bash
 
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+apt-get install python-requests
+apt-get install python-dnspython
+apt-get install python-argparse
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+sudo apt-get install egrep
+sudo apt-get install snap
+sudo apt-get install snapd
+sudo apt-get install -y xargs
+sudo snap install amass
+sudo apt-get install cargo
+
+#pip / pip3 install
+pip3 install Pool
+pip3 install requests
+pip3 install codecs
+pip3 install time
+
+#install go
+if [[ -z "$GOPATH" ]];then
+echo "It looks like go language is not installed, would you like to install it now"
+PS3="Please select an option : "
+choices=("yes" "no")
+select choice in "${choices[@]}"; do
+        case $choice in
+                yes)
+
+					echo "Installing Golang"
+					wget https://golang.org/dl/go1.15.linux-amd64.tar.gz
+					sudo tar -xvf go1.15.linux-amd64.tar.gz
+					sudo mv go /usr/local
+					export GOROOT=/usr/local/go
+					export GOPATH=$HOME/go
+					export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+					echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+					echo 'export GOPATH=$HOME/go'	>> ~/.bashrc		
+					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bashrc
+					source ~/.bashrc
+					sleep 1
+					break
+					;;
+				no)
+					echo "Please install go and rerun this script"
+					echo "Aborting installation..."
+					exit 1
+					;;
+	esac	
+done
+fi
+
 #create a ali folder in ~/
 mkdir ~/ali
 cd ~/ali/
